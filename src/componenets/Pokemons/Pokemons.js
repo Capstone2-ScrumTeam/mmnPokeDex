a4ad1448e1b7d4e60554ba17aeead4df9d035385
Ning Li <n.li9402@gmail.com>
2021-06-23 16:35:21 -0400

    changes to appjs v6

--- src/componenets/Pokemons/Pokemons.js
+++ src/componenets/Pokemons/Pokemons.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
-import PokeList from '../PokeList/PokeList';
+import React, { Component } from "react";
+import PokeList from "../PokeList/PokeList";
 
 class Pokemons extends Component {
-    render() {
-        return (
-            <section>
-              <h2>Pokemons!</h2>
-              <PokeList />
-            </section>
-          );
-        }
-      }
+  render() {
+    return (
+      <section>
+        <h2>Pokemons!</h2>
+        <PokeList />
+      </section>
+    );
+  }
+}
 export default Pokemons;
