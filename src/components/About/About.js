982e7abc0955c0d8cea998a374aa9288e040705b
Ning Li <n.li9402@gmail.com>
2021-06-24 15:16:04 -0400

    changes to about.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,18 +1,63 @@
 import React, { Component } from "react";
 import "./About.css";
 
-function Team(props) {
-  return <h1 className="bgpicture">This was done by {props.name}</h1>;
-}
-
 class About extends Component {
   render() {
     return (
       <div className="aboutpage">
-        <h1>Meet the Team</h1>
-        <Team name="Mimi" />
-        <Team name="Moe" />
-        <Team name="Ning" />
+        <div className="about-section">
+          <h1>About Us Page</h1>
+          <p>Some text about who we are and what we do.</p>
+          <p>
+            Resize the browser window to see that this page is responsive by the
+            way.
+          </p>
+        </div>
+        <h2 className="Teamtitle">Our Team</h2>
+        <div className="row">
+          <div className="column">
+            <div className="card">
+              <img src="" alt="Mimi" />
+              <div className="container">
+                <h2>Mimi</h2>
+                <p className="title">Scrum Master</p>
+                <p>Some information about them</p>
+                <p>Email goes here</p>
+                <p>
+                  <button className="button">Contact</button>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="column">
+            <div className="card">
+              <img src="" alt="Moe" />
+              <div className="container">
+                <h2>Moe</h2>
+                <p className="title">Team Member</p>
+                <p>Some information about them</p>
+                <p>Email goes here</p>
+                <p>
+                  <button className="button">Contact</button>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="column">
+            <div className="card">
+              <img src="" alt="Ning" />
+              <div className="container">
+                <h2>Ning</h2>
+                <p className="title">Team Member</p>
+                <p>Some information about them</p>
+                <p>Email goes here</p>
+                <p>
+                  <button className="button">Contact</button>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
